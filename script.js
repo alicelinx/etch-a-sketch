211cc653b5e011c232c05f84cf7f4de0332538b0
Alice Lin <alicexxlin@gmail.com>
2023-08-31 16:14:17 -0600

    implement  hover effect and function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const gridContainer = document.querySelector('.grid-container');
+const grids = document.querySelectorAll('.grid');
+
+const applyHoverEffect = (e) => {
+  e.target.classList.add(':hover');
+};
 
 for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement('div');
   div.classList.add('grid');
   gridContainer.appendChild(div);
 }
+
+grids.forEach(grid => grid.addEventListener('mouseenter', applyHoverEffect));
