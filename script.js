5449bd646f406f5514dd724042dc1883fb1d6275
Alice Lin <alicexxlin@gmail.com>
2023-09-01 12:24:09 -0600

    add click event

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const gridContainer = document.querySelector('.grid-container');
 const grids = document.querySelectorAll('.grid');
 
 const applyHoverEffect = (e) => {
-  e.target.classList.add(':hover');
+  if (e.target.classList.contains('grid')) {
+    e.target.classList.add(':hover');
+  }
+};
+
+const applyColor = (e) => {
+  if (e.target.classList.contains('grid:hover')) {
+    e.target.classList.remove('grid:hover').add('grid-clicked');
+  }
 };
 
 for (let i = 0; i < 16 * 16; i++) {
@@ -11,4 +19,5 @@ for (let i = 0; i < 16 * 16; i++) {
   gridContainer.appendChild(div);
 }
 
+grids.forEach(grid => grid.addEventListener('click', applyColor));
 grids.forEach(grid => grid.addEventListener('mouseenter', applyHoverEffect));
